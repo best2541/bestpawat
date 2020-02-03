71601a7fc40af718b2146ee285e80ac23b272230
Krissada Boontrigratn <krissada@appsynth.net>
2020-02-03 19:45:19 +0700

    restructure folder and file

--- app/routes/product.js
+++ app/routes/product.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('product',{ title:'สินค้า'});
+router.get('/', function (req, res, next) {
+  res.render('product', { title: 'สินค้า' });
 });
 
 module.exports = router;
